40b9ac788b3f9a8933fb46006cbcd1a708910f56
ST10063915 <ryan@dunnix.co.za>
2024-05-16 12:43:16 +0200

    Initial commit

---
+++ app/src/test/java/com/opsc/icetask4/ExampleUnitTest.kt
@@ -0,0 +1,21 @@
+package com.opsc.icetask4
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+    @Test
+    fun multiplication_isCorrect() {
+        assertEquals(6, 2 * 3)
+    }
+    @Test
+    fun division_isCorrect() {
+        assertEquals(3, 9 / 3)
+    }
+}
